bf57d52abeb807254550c7722bedfa452ae101c7
Ryan Porter <58050063+RyPort@users.noreply.github.com>
2021-04-19 14:08:05 -0400

    added num tuples

--- CIS4301 Project - Baseball Trends/ClientApp/src/components/Home.js
+++ CIS4301 Project - Baseball Trends/ClientApp/src/components/Home.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
   static displayName = Home.name;
-
+    shoot() {
+        alert('318948');
+    }
   render () {
     return (
       <div>
         <h1>Welcome to Baseball Trends</h1>
-        <p>Click on any of the pages above to see a graphical representation of an interesting baseball trend</p>z
+            <p>Click on any of the pages above to see a graphical representation of an interesting baseball trend</p>
+            <button onClick={this.shoot}>Number of Tuples</button>
       </div>
     );
   }
